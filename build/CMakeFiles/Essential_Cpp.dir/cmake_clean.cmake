c52c197aef2b120207e02601df10ef154f5eb021
lil7abt <lili.0909@outlook.de>
2020-08-12 13:41:59 +0200

    Mac: build folder outside the k1 folder

---
+++ build/CMakeFiles/Essential_Cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Essential_Cpp.dir/hello.cpp.o"
+  "Essential_Cpp"
+  "Essential_Cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Essential_Cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
